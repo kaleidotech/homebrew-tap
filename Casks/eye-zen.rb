5055d792046ed7719e6c6fdbb3efcc46dc336558
Kaleido <86684243+kaleidotech@users.noreply.github.com>
2025-09-10 16:58:54 +0200

    bump

--- Casks/eye-zen.rb
+++ Casks/eye-zen.rb
@@ -1,5 +1,5 @@
 cask "eye-zen" do
-  version "1.0.0"
+  version "1.0.5"
   sha256 "sha256:578b1ddbe705bd7927c03692cb24a0f39d2c0a75a72764af6d64c02d3ec06833"
 
   url "https://github.com/kaleidotech/eye-zen/releases/download/#{version}/eye_zen_darwin_arm64.zip"
